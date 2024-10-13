d1c48d59fbe847c268014fc2277ef4783c3f8ca6
PritamBiswas489 <pritam.biswas489@gmail.com>
2024-10-13 20:43:07 +0530

    13-10-2024-01

--- src/services/payment.service.tsx
+++ src/services/payment.service.tsx
@@ -9,7 +9,7 @@ interface Response {
 	request?: any;
 }
 
-
+//create order rozar pay service
 export const createOrderRozarPayService = async(formData:FormData): Promise<Response> => {
 	 
 	try {
@@ -19,6 +19,7 @@ export const createOrderRozarPayService = async(formData:FormData): Promise<Resp
 		return error.response;
 	}
 }
+// payment success process
 export const paymentSuccessProcess = async(formData:FormData): Promise<Response> => {
 	try {
 		const response = await api.post(`/provider/razor-pay-success-order`,formData);
@@ -26,4 +27,14 @@ export const paymentSuccessProcess = async(formData:FormData): Promise<Response>
 	} catch (error:any) {
 		return error.response;
 	}
+}
+// payment listing 
+export const paymentListing  = async(limit:number,offset:number):Promise<Response> => {
+	try {
+        const response = await api.get(`/provider/payment-list?limit=${limit}&offset=${offset}`);
+        return response;
+    } catch (error:any) {
+        return error.response;
+    }
+
 }
